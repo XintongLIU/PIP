3863c52a41617ba6da3f859549377b7c3e4db3b1
xueyifei <787706202@qq.com>
2016-06-01 21:09:29 +0800

    StudentDAOImpl and projectScoreDAOImpl

--- src/com/pip/dao/IProjectScoreDAO.java
+++ src/com/pip/dao/IProjectScoreDAO.java
@@ -5,6 +5,6 @@ import com.pip.domain.ProjectScore;
 public interface IProjectScoreDAO{
 	public List<ProjectScore> findAll();		//返回所有的ProjectScore信息
 	public ProjectScore findProjectScoreById(int projectID);		//通过ProjectScoreID查找ProjectScore信息
-	public boolean insertProjectScore(ProjectScore ProjectScore);			//插入ProjectScore信息
-	public boolean deleteProjectScore(ProjectScore ProjectScore);
+	public Integer insertProjectScore(ProjectScore projectScore);			//插入ProjectScore信息
+	public void deleteProjectScore(ProjectScore projectScore);
 }
--- src/com/pip/dao/impl/StudentDAOImpl.java
+++ src/com/pip/dao/impl/StudentDAOImpl.java
@@ -3,88 +3,46 @@ package com.pip.dao.impl;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.pip.dao.IStudentDAO;
 import com.pip.domain.Student;
-@Repository
-public class StudentDAOImpl extends BaseDAOImpl<Student> implements IStudentDAO{
 
-	@Override
-	public List<Student> find(String hql, Student[] param) {
-		// TODO Auto-generated method stub
-		return super.find("from Student where studentID= :param", param);
-	}
-
-	@Override
-	public Student get(String hql, Student[] param) {
-		// TODO Auto-generated method stub
-		return super.get(Student.class, param);
-	}
-
-	@Override
-	public Long count(String hql, Student[] param) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Integer executeHql(String hql, Student[] param) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+public class StudentDAOImpl extends HibernateDaoSupport implements IStudentDAO{
 
 	@Override
 	public List<Student> findAll() {
 		// TODO Auto-generated method stub
-		List<Student> list = new ArrayList<Student>();
-		list = super.find("from Student");
-		return list;
+		return (List<Student>) getHibernateTemplate().find("from Student");
 	}
 
 	@Override
 	public Student findStudentById(int studentID) {
 		// TODO Auto-generated method stub
-		List<Student> list = super.find("from Student where studentID = " + studentID);
+		return (Student) getHibernateTemplate().get(Student.class, studentID);
+	}
+
+	@Override
+	public Integer insertStudent(Student student) {
+		// TODO Auto-generated method stub
+		return (Integer) getHibernateTemplate().save(student);
+	}
+
+	@Override
+	public void deleteStudent(Student student) {
+		// TODO Auto-generated method stub
+		getHibernateTemplate().delete(student);
+	}
+
+	@Override
+	public Student findStudentByName(String studentName) {
+		// TODO Auto-generated method stub
+		List<Student> list = getHibernateTemplate().find("from Student as s where s.studentName=?", studentName); 
 		return list.get(0);
 	}
 
-	@Override
-	public boolean insertStudent(Student student) {
-		// TODO Auto-generated method stub
-		try{
-			super.save(student);
-			return true;
-		}catch(Exception e){
-			e.printStackTrace();
-			return false;
-		}
-	}
-
-	@Override
-	public boolean deleteStudent(Student student) {
-		// TODO Auto-generated method stub
-		try{
-//			super.delete(student);
-			super.executeHql("delete from Student where studentID = " + student.getStudentID());
-			return true;
-		}catch(Exception e){
-			e.printStackTrace();
-			return false;
-		}
-	}
 	
-
-	public List<Student> findStudentByTeamID(Integer teamID){
-		List<Student> list = new ArrayList<Student>();
-		list = super.find("from Student where teamID = " + teamID);
-		return list;
-	}
-		
-	public Student findStudentByName(String studentName){
-		List<Student> list = super.find("from Student where studentName = " + studentName);
-		return list.get(0);
-	}
 
 }
