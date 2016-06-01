12b85ba209e020c481f5eea92d2a1b6e53b1cab2
刘昕彤 <lxtlovol@163.com>
2016-06-01 16:42:35 +0800

    Add TeacherService

---
+++ src/com/pip/service/impl/TeacherServiceImpl.java
@@ -0,0 +1,38 @@
+package com.pip.service.impl;
+
+import java.util.List;
+
+import com.pip.domain.Teacher;
+import com.pip.service.ITeacherService;
+import com.pip.dao.impl.TeacherDAOImpl;
+
+public class TeacherServiceImpl implements ITeacherService {
+
+	TeacherDAOImpl teacherDao;
+
+	@Override
+	public int insertTearcher(Teacher teacher) {
+		return (Integer)teacherDao.save(teacher);
+	}
+
+	@Override
+	public void deleteTeacher(int id) {
+		teacherDao.deleteTeacher(teacherDao.findTeacherById(id));
+	}
+
+	@Override
+	public void updateTeacher(Teacher teacher) {
+		teacherDao.update(teacher);
+	}
+
+	@Override
+	public Teacher selectTeacher(int id) {
+		return teacherDao.findTeacherById(id);
+	}
+
+	@Override
+	public List<Teacher> showTeacherList() {	
+		return teacherDao.findAll();
+	}
+
+}
