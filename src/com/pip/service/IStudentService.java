743eb066e37c93ff3c3c3c6c9a484bcfe67360b1
saber4869 <saber4869@outlook.com>
2016-06-01 16:39:14 +0800

    adminService+studentService

---
+++ src/com/pip/service/IStudentService.java
@@ -0,0 +1,14 @@
+package com.pip.service;
+
+import java.util.List;
+
+import com.pip.domain.Student;
+
+public interface IStudentService {
+	Integer insertStudent(Student student);
+	void deleteStudent(Integer id);
+	void updateStudent(Student student);
+	Student selectStudent(Integer id);
+	List<Student> showStudentList();
+	
+}
