9b946b29e0815fb8319bc47d0ff5377997c5c65d
刘昕彤 <lxtlovol@163.com>
2016-06-05 09:52:28 +0800

    Update AddTeacherAction

--- src/com/pip/action/AddTeacherAction.java
+++ src/com/pip/action/AddTeacherAction.java
@@ -1,21 +1,29 @@
 package com.pip.action;
 
+import java.util.List;
+
 import com.pip.service.impl.TeacherServiceImpl;
 import com.pip.domain.Teacher;
 
 public class AddTeacherAction {
 
 	TeacherServiceImpl teacherService;
+	String teacherNum;
 	Integer teacherID;
 	String teacherName;
 	Teacher teacher;
-	
+	List<Teacher> teacherList;
 	public String execute(){
+
+		if(teacherID==null||teacherName==null)
+			return "fail";
+		teacherID = Integer.parseInt(teacherNum);
 		teacher = new Teacher();
 		teacher.setTeacherName(teacherName);
 		teacher.setPassword("123456");
 		teacher.setTeacherID(teacherID);
 		teacherService.insertTearcher(teacher);
+		teacherList = teacherService.showTeacherList();
 		return "success";
 	}
 
@@ -49,5 +57,21 @@ public class AddTeacherAction {
 
 	public void setTeacher(Teacher teacher) {
 		this.teacher = teacher;
+	}
+
+	public String getTeacherNum() {
+		return teacherNum;
+	}
+
+	public void setTeacherNum(String teacherNum) {
+		this.teacherNum = teacherNum;
+	}
+
+	public List<Teacher> getTeacherList() {
+		return teacherList;
+	}
+
+	public void setTeacherList(List<Teacher> teacherList) {
+		this.teacherList = teacherList;
 	}
 }
