5774f04ffeb65bfd1843d26dfb232ccfff1115c7
刘昕彤 <lxtlovol@163.com>
2016-06-05 10:02:18 +0800

    Update UpdateTeacherAction

--- src/com/pip/action/UpdateTeacherAction.java
+++ src/com/pip/action/UpdateTeacherAction.java
@@ -1,31 +1,33 @@
 package com.pip.action;
 
+import java.util.List;
+
+import com.pip.dao.ITeacherDAO;
 import com.pip.domain.Teacher;
+import com.pip.service.ITeacherService;
 import com.pip.service.impl.TeacherServiceImpl;
 
 public class UpdateTeacherAction {
 	
-	TeacherServiceImpl teacherService;
+	ITeacherService teacherService;
 	Teacher teacher;
+	String teacherNum;
 	int teacherID;
 	String teacherName;
-	String teacherPassword;
+	List<Teacher> teacherList;
+	ITeacherDAO teacherDAO;
 	
 	public String execute(){
+		teacherID = Integer.parseInt(teacherNum);
+		teacher = teacherService.selectTeacher(teacherID);
 		teacher.setTeacherID(teacherID);
 		teacher.setTeacherName(teacherName);
-		teacher.setPassword(teacherPassword);
 		teacherService.updateTeacher(teacher);
+		teacherList = teacherService.showTeacherList();
 		return "success";
 	}
 
-	public TeacherServiceImpl getTeacherService() {
-		return teacherService;
-	}
 
-	public void setTeacherService(TeacherServiceImpl teacherService) {
-		this.teacherService = teacherService;
-	}
 
 	public Teacher getTeacher() {
 		return teacher;
@@ -51,12 +53,54 @@ public class UpdateTeacherAction {
 		this.teacherName = teacherName;
 	}
 
-	public String getTeacherPassword() {
-		return teacherPassword;
+
+
+	public ITeacherService getTeacherService() {
+		return teacherService;
 	}
 
-	public void setTeacherPassword(String teacherPassword) {
-		this.teacherPassword = teacherPassword;
+
+
+	public void setTeacherService(ITeacherService teacherService) {
+		this.teacherService = teacherService;
 	}
+
+
+
+	public String getTeacherNum() {
+		return teacherNum;
+	}
+
+
+
+	public void setTeacherNum(String teacherNum) {
+		this.teacherNum = teacherNum;
+	}
+
+
+
+	public List<Teacher> getTeacherList() {
+		return teacherList;
+	}
+
+
+
+	public void setTeacherList(List<Teacher> teacherList) {
+		this.teacherList = teacherList;
+	}
+
+
+
+	public ITeacherDAO getTeacherDAO() {
+		return teacherDAO;
+	}
+
+
+
+	public void setTeacherDAO(ITeacherDAO teacherDAO) {
+		this.teacherDAO = teacherDAO;
+	}
+
+
 
 }
